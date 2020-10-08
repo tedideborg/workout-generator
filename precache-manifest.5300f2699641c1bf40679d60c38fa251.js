234b1c82e02add2901e3122df2e34f130c2442ff
Ted Ideborg <t.ideborg@gmail.com>
2020-10-08 22:03:51 +0200

    Updates

---
+++ precache-manifest.5300f2699641c1bf40679d60c38fa251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70cb0646fdf363c37a35904f0f25274",
+    "url": "/workout-generator/index.html"
+  },
+  {
+    "revision": "67f2a0573216b5b9ceef",
+    "url": "/workout-generator/static/css/main.a18a6f14.chunk.css"
+  },
+  {
+    "revision": "0efce9d27aa61e65040d",
+    "url": "/workout-generator/static/js/2.be026125.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/workout-generator/static/js/2.be026125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f2a0573216b5b9ceef",
+    "url": "/workout-generator/static/js/main.f72b6f5c.chunk.js"
+  },
+  {
+    "revision": "a8fec3351026bbb28666",
+    "url": "/workout-generator/static/js/runtime-main.52ae8237.js"
+  }
+]);
